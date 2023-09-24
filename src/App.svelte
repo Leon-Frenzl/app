0d414d701ecc8d98e6cda913172f12d90fcb15cc
Leon-Frenzl <leonfrenzl@googlemail.com>
2023-09-24 14:35:20 +0200

    Routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./lib/routes/Home.svelte";
+  import Blog from "./lib/routes/Blog.svelte";
+  import About from "./lib/routes/About.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-</main>
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/blog">Blog</Link>
+    <Link to="/about">About</Link>
+  </nav>
+  <Route path="/" component={Home} />
+  <Route path="/blog" component={Blog} />
+  <Route path="/about" component={About} />
+</Router>
 
 <style>
 </style>
