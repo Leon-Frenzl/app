c1075d93f58d9cc10838f3c3a3f9224c953ed328
Leon-Frenzl <leonfrenzl@googlemail.com>
2023-09-24 15:30:44 +0200

    New: Layout and Beginning

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,42 @@
 <script>
-  import { Router, Link, Route } from "svelte-routing";
-  import Home from "./lib/routes/Home.svelte";
-  import Blog from "./lib/routes/Blog.svelte";
-  import About from "./lib/routes/About.svelte";
-
-  export let url = "";
 </script>
 
-<Router {url}>
-  <nav>
-    <Link to="app/">Home</Link>
-    <Link to="app/blog">Blog</Link>
-    <Link to="app/about">About</Link>
-  </nav>
-  <Route path="app/" component={Home} />
-  <Route path="/app/blog" component={Blog} />
-  <Route path="/app/about" component={About} />
-</Router>
+<main>
+  <div class="left">
+    <div class="image">
+      <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="">
+    </div>
+    <div class="description">
+      <h1>Name</h1>
+      <h1>Name</h1>
+      <h1>Name</h1>
+      <h1>Name</h1>
+    </div>
+  </div>
+  <div class="right">Right Side</div>
+</main>
 
 <style>
+  h1{
+    margin-left: 6vh;
+  }
+  img{
+    object-fit: cover;
+    width: 70%;
+    margin-left: 6vh;
+    margin-top: 4vh;
+    padding-bottom: 2vh;
+  }
+  .left{
+    float: left;
+    width: 20%;
+    height: 100vh;
+    background-color: blue;
+  }
+  .right{
+    float: right;
+    width: 80%;
+    height: 100vh;
+    background-color: red;
+  }
 </style>
